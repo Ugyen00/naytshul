aa77b0f0bc428287f762d997a96dd4d67c80b335
Sanam_App <ugyendechen00@gmail.com>
2024-09-11 20:32:25 +0600

    home and title

--- src/pages/Headlines.js
+++ src/pages/Headlines.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Headlines = () => {
     return (
-        <div className="flex justify-center items-center h-screen bg-gray-100">
+        <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
             <h1 className="text-4xl font-bold text-blue-600">Headlines Page</h1>
+            <p className="text-2xl font-bold text-green-400 mt-4">We Share Facts, Not Opinions</p>
         </div>
     );
 };
