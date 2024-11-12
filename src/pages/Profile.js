c00c46a8ae6fae531b744217ec9bf4e4a9993217
Tsheltrim Pemo <tsheltrimpemo2004@gmail.com>
2024-11-12 21:28:18 +0600

    Profile_responsive

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -45,40 +45,40 @@ const Profile = () => {
   const [activeTab, setActiveTab] = useState('liked'); // 'liked' or 'commented'
 
   return (
-    <div className="bg-gray-100 lg:ml-64 ml-20">
-      <div className='flex p-12 space-x-16'>
+    <div className="bg-gray-100 lg:ml-64 ml-6">
+      <div className="flex flex-col lg:flex-row p-6 lg:p-12 space-y-8 lg:space-y-0 lg:space-x-16 items-center">
         <img
-          src='/user1.png'
-          className='w-48 h-48 rounded-full'
-          alt='User Profile'
+          src={user.profileImage}
+          className="w-32 h-32 lg:w-48 lg:h-48 rounded-full"
+          alt="User Profile"
         />
 
-        <div className='border border-[#CCCCCC] rounded-lg px-10 py-4 w-[800px] h-auto'>
-          <div className='flex justify-between'>
-            <h1 className='text-xl'>Personal Info</h1>
-            <button className='py-2 px-4 bg-[#66C564] text-white rounded-xl'>Edit Profile</button>
+        <div className="border border-[#CCCCCC] rounded-lg px-6 py-4 lg:px-10 lg:py-4 w-full lg:w-[800px]">
+          <div className="flex justify-between items-center">
+            <h1 className="text-md lg:text-xl">Personal Info</h1>
+            <button className="py-2 px-4 bg-[#66C564] text-white rounded-xl text-sm lg:text-base">
+              Edit Profile
+            </button>
           </div>
-          <div className='flex flex-row justify-around mt-8'>
-            <div className='flex flex-col'>
-              <p className='text-[#888888]'>Name</p>
+          <div className="flex flex-col lg:flex-row lg:justify-around mt-6 space-y-4 lg:space-y-0">
+            <div className="flex flex-col items-center lg:items-start">
+              <p className="text-[#888888]">Name</p>
               <p>{user.name}</p>
             </div>
-
-            <div className='flex flex-col'>
-              <p className='text-[#888888]'>Email</p>
+            <div className="flex flex-col items-center lg:items-start">
+              <p className="text-[#888888]">Email</p>
               <p>{user.email}</p>
             </div>
-
-            <div className='flex flex-col'>
-              <p className='text-[#888888]'>Joined On</p>
+            <div className="flex flex-col items-center lg:items-start">
+              <p className="text-[#888888]">Joined On</p>
               <p>{user.joinedOn}</p>
             </div>
           </div>
         </div>
       </div>
 
-      <div className='p-12'>
-        <div className='flex flex-row space-x-8 border-b-2 pb-4'>
+      <div className="p-6 lg:p-12">
+        <div className="flex space-x-8 border-b-2 pb-4 justify-center lg:justify-start">
           <button
             className={`flex items-center space-x-2 ${activeTab === 'liked' ? 'text-[#DEBCF4]' : 'text-black'}`}
             onClick={() => setActiveTab('liked')}
@@ -95,25 +95,25 @@ const Profile = () => {
 
         {activeTab === 'liked' && (
           <>
-            <p className='text-[#807E7E] p-4'>{articles.length} Likes</p>
+            <p className="text-[#807E7E] p-4">{articles.length} Likes</p>
             {articles.map((article) => (
-              <div key={article.id} className='bg-[#292929] rounded-md py-4 px-8 text-white mt-4'>
-                <div className='flex space-x-2'>
+              <div key={article.id} className="bg-[#292929] rounded-md py-4 px-6 lg:px-8 text-white mt-4">
+                <div className="flex items-center space-x-2">
                   <img
-                    src='/cnn.png'
-                    className='w-6 h-6'
-                    alt='Source Logo'
+                    src="/cnn.png"
+                    className="w-6 h-6"
+                    alt="Source Logo"
                   />
-                  <h1 className='text-white text-sm'>{article.source}</h1>
+                  <h1 className="text-white text-sm">{article.source}</h1>
                 </div>
-                <div className='flex flex-row justify-between'>
-                  <p className='pt-2'>{article.title}</p>
-                  <div className='flex flex-row space-x-4'>
-                    <button className='flex items-center space-x-2 text-[#DEBCF4]'>
-                      <span className='text-white'>{article.likes}</span> <FaHeart />
+                <div className="flex flex-col lg:flex-row justify-between mt-2 space-y-2 lg:space-y-0">
+                  <p>{article.title}</p>
+                  <div className="flex space-x-4">
+                    <button className="flex items-center space-x-2 text-[#DEBCF4]">
+                      <span className="text-white">{article.likes}</span> <FaHeart />
                     </button>
-                    <button className='text-[#DEBCF4]'><FiMessageCircle /></button>
-                    <button className='text-[#DEBCF4]'><FiShare2 /></button>
+                    <button className="text-[#DEBCF4]"><FiMessageCircle /></button>
+                    <button className="text-[#DEBCF4]"><FiShare2 /></button>
                   </div>
                 </div>
               </div>
@@ -122,55 +122,53 @@ const Profile = () => {
         )}
 
         {activeTab === 'commented' && (
-          <div className='p-4'>
-            <p className='text-[#807E7E] p-4'>{comments.length} Comments</p>
+          <div className="p-4">
+            <p className="text-[#807E7E] p-4">{comments.length} Comments</p>
             {comments.map((comment) => (
-              <div key={comment.id} className='bg-[#292929] rounded-md py-4 px-8 text-white mt-4'>
-                <div className='flex items-start space-x-4'>
-              <img
-                 src='/cnn.png'
-                className='w-8 h-8 rounded-full'
-                alt='User Profile'
-              />
-              <div className='flex-1'>
-                <div className='flex justify-between'>
-                  <div>
-                    <h1 className='text-white text-sm'>{comment.source}</h1>
-                    <p className='text-white italic'>{comment.title}</p>
-                  </div>
-                  <div className='flex items-center space-x-4'>
-                    <span className='flex items-center text-[#DEBCF4]'>
-                      <span className='text-white mr-2'>{comment.likes}</span> <FaHeart />
-                    </span>
-                    <button className='text-[#DEBCF4]'><FiMessageCircle /></button>
-                    <button className='text-[#DEBCF4]'><FiShare2 /></button>
-                  </div>
-                </div>
-
-                <div className='flex space-x-4 mt-4'>
+              <div key={comment.id} className="bg-[#292929] rounded-md py-4 px-4 lg:px-8 text-white mt-4">
+                <div className="flex flex-col lg:flex-row items-start lg:space-x-4 space-y-4 lg:space-y-0">
                   <img
-                      src={user.profileImage}
-                      className='w-12 h-12 rounded-full'
-                      alt='User Profile'
+                    src="/cnn.png"
+                    className="w-8 h-8 rounded-full"
+                    alt="Source Logo"
                   />
-                  <div className='flex-1'>
-                    <div className='flex space-x-2'>
-                      <p>{user.name}</p>
-                      <p className='text-[#807E7E]'>{comment.time}</p>
+                  <div className="flex-1">
+                    <div className="flex flex-col lg:flex-row justify-between">
+                      <div>
+                        <h1 className="text-white text-sm">{comment.source}</h1>
+                        <p className="text-white italic">{comment.title}</p>
+                      </div>
+                      <div className="flex items-center mt-2 lg:mt-0 space-x-4">
+                        <span className="flex items-center text-[#DEBCF4]">
+                          <span className="text-white mr-2">{comment.likes}</span> <FaHeart />
+                        </span>
+                        <button className="text-[#DEBCF4]"><FiMessageCircle /></button>
+                        <button className="text-[#DEBCF4]"><FiShare2 /></button>
+                      </div>
                     </div>
-                  <p className='mt-4'>{comment.comment}</p>
-                  <div className='flex mt-2 text-[#807E7E]'>
-                  <div className='flex space-x-4'>
-                    <button>Reply</button>
-                    <button>Edit</button>
-                    <button>Delete</button>
-                  </div>
-                </div>
-                  </div>
-                </div>
 
-              </div>
-            </div>
+                    <div className="flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4 mt-4">
+                      <img
+                        src={user.profileImage}
+                        className="w-10 h-10 rounded-full"
+                        alt="User Profile"
+                      />
+                      <div className="flex-1">
+                        <div className="flex flex-col lg:flex-row lg:space-x-2">
+                          <p>{user.name}</p>
+                          <p className="text-[#807E7E]">{comment.time}</p>
+                        </div>
+                        <p className="mt-2">{comment.comment}</p>
+                        <div className="flex mt-2 text-[#807E7E] space-x-4">
+                          <button>Reply</button>
+                          <button>Edit</button>
+                          <button>Delete</button>
+                        </div>
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
               </div>
             ))}
           </div>
@@ -181,6 +179,3 @@ const Profile = () => {
 };
 
 export default Profile;
-
-
-
